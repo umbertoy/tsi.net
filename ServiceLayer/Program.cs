bbe4e963fab1ec8d9bbcfe028030771f2b9c3e91
umbertoy <umbertoyobs@gmail.com>
2016-04-02 16:50:29 -0300

    Arreglo cwf

--- ServiceLayer/Program.cs
+++ ServiceLayer/Program.cs
@@ -31,11 +31,16 @@ namespace ServiceLayer
             binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
             binding.Security.Mode = BasicHttpSecurityMode.None;
 
-            Uri baseAddress = new Uri("http://127.0.0.1:8181/tsi-net-01/service");
-            Uri address = new Uri("http://127.0.0.1:8181/tsi-net-01/service/Employees");
+            Uri baseAddress = new Uri("http://127.0.0.1:8181/tsi-net-01-service");
+            Uri address = new Uri("http://127.0.0.1:8181/tsi-net-01-service/Employees");
 
             ServiceHost serviceHost = new ServiceHost(typeof(ServiceEmployees), baseAddress);
             serviceHost.AddServiceEndpoint(typeof(IServiceEmployees), binding, address);
+
+            ServiceMetadataBehavior metadatos = new ServiceMetadataBehavior();
+            metadatos.HttpGetEnabled = true;
+            serviceHost.Description.Behaviors.Add(metadatos);
+
             serviceHost.Open();
 
             // The service can now be accessed.
