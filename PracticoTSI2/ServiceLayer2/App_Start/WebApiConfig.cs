0f34d7f09b2485f8bf740d66447e3428a03f5302
umbertoy <umbertoyobs@gmail.com>
2016-04-07 18:29:34 -0300

    Practico 2

--- PracticoTSI2/ServiceLayer2/App_Start/WebApiConfig.cs
+++ PracticoTSI2/ServiceLayer2/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace ServiceLayer2
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
-
+            config.EnableCors();
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
