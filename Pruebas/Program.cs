98325acc5946dc3404bc84cb0d6a65d3e3577e23
Camilo Pereyra <camilopereyraecheverz@gmail.com>
2016-04-06 23:10:30 -0300

    cambio del program.cs (a ver si se destranca esto)

--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -22,11 +22,19 @@ namespace Pruebas
             dal.AddEmployee(part);
             DALEmployeesMongo dal = new DALEmployeesMongo();
             dal.DeleteEmployee(5);*/
-            DALEmployeesMongo dal = new DALEmployeesMongo();
+            DALEmployeesEF dal = new DALEmployeesEF();
+            FullTimeEmployee saveEmp = new FullTimeEmployee();
+            saveEmp.Name = "RoboMongo";
+            saveEmp.StartDate = DateTime.UtcNow.Date;
+            saveEmp.Salary = 1000;
+            dal.AddEmployee(saveEmp);
             Employee emp = dal.GetEmployee(1);
+            
+            System.Console.WriteLine("id del puto este: " + emp.Id);
+            System.Console.WriteLine("nombre del puto este: "+ emp.Name);
+            System.Console.WriteLine("el puto este arranco: " + emp.StartDate);
+            System.Console.WriteLine("el puto este cobra: " + ((FullTimeEmployee)emp).Salary);
 
-
-            System.Console.WriteLine("hola mundo "+args.Length);
         }
     }
 }
