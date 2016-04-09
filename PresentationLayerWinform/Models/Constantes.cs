f8b56e33e4b6cbb087508d85b50ae6805d63b42b
umbertoy <umbertoyobs@gmail.com>
2016-04-08 23:02:23 -0300

    No se que hacer, lo siento

---
+++ PresentationLayerWinform/Models/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayerWinform.Models
+{
+    class Constantes
+    {
+        public static String StrFullTimeEmployee = "Full-Time";
+        public static String StrPartTimeEmployee = "Part-Time";
+
+    }
+}
