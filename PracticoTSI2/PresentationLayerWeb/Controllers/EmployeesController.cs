9b0eb05b59686685ff2a43d1ad4250ee1c5ba4b1
umbertoy <umbertoyobs@gmail.com>
2016-04-10 17:54:24 -0300

    Capa de presentacion web

---
+++ PracticoTSI2/PresentationLayerWeb/Controllers/EmployeesController.cs
@@ -0,0 +1,93 @@
+﻿using BusinessLogicLayer;
+using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayerWeb.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private IBLEmployees blHandler = new BLEmployees(new DataAccessLayer.DALEmployeesEF());
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<Employee> listEmployees = blHandler.GetAllEmployees();
+            return View(listEmployees);
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(blHandler.GetEmployee(id));
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
